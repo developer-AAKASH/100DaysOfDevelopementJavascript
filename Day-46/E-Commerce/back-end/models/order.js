3206156ba67853badea00111947b4454b9eccf83
developer-AAKASH <201912082@daiict.ac.in>
2021-02-16 20:38:51 +0530

    Commit with code of Day-46 ---I complete my all models for now based on my database table...

---
+++ Day-46/E-Commerce/back-end/models/order.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema; 
+
+const productCartSchema = new mongoose.Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: {
+        type: String
+    },
+    count: {
+        type: Number
+    },
+    price: {
+        type: Number
+    }
+});
+
+const ProductCart = mongoose.model( "ProductCart", productCartSchema );
+
+const orderSchema = new mongoose.Schema({
+    products: [ productCartSchema ],
+    transactionId: {
+
+    },
+    amount: {
+        type: Number
+    },
+    address: {
+        type: String
+    },
+    updated: {
+        type: Date,
+    },
+    user: {
+        type: ObjectId,
+        ref: "User"
+    }
+},
+{
+    timestamps: true
+});
+
+const Order = mongoose.model( "Order", orderSchema );
+
+module.exports = { ProductCart, Order };
