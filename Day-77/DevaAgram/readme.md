585744925874221e6a234b9162b09bbf974ef0bb
developer-AAKASH <201912082@daiict.ac.in>
2021-03-19 12:27:39 +0530

    Commit with code of Day-77, Today I start with setting up my redux structure and I add signIn, SignUp, SignOut functionality using firebase auth and database.

---
+++ Day-77/DevaAgram/readme.md
@@ -0,0 +1,9 @@
+So on Day-76, we configured everything, now its time to write code so from today onward, I just provide "src" folder in which, my all code file and folder structure will be there rest everything will be same.
+
+Any change will be added on readme file for that respected day.
+
+Today, In android/app/build.gradle, I make one more change to avoid errors !!
+
+Inside default config,
+
+Add, "multiDexEnabled true" this line after versioName line.
---
+++ Day-77/DevaAgram/src/action/auth.js
@@ -0,0 +1,87 @@
+import auth from "@react-native-firebase/auth";
+import Snackbar from "react-native-snackbar";
+import database from '@react-native-firebase/database';
+
+export const signUp = ( data )=>async( dispatch )=>{
+    console.log(data);
+    const { name, instaUserName, bio, email, password, country, image } = data;
+
+    auth()
+    .createUserWithEmailAndPassword( email, password )
+    .then(( data )=>{
+        console.log(data);
+        console.log("User created succesfuly !!");
+
+        database()
+        .ref('/users/' + data.user.uid )
+        .set({
+            uid: data.user.uid,
+            name, 
+            instaUserName,
+            country,
+            image,
+            bio
+        })
+        .then(()=> console.log("Data set done !!"));
+
+        Snackbar.show({
+            text: "Account created",
+            textColor: "green",
+            backgroundColor: "#1b262c"
+        });
+    })
+    .catch( (error) => {
+        console.error( error );
+        Snackbar.show({
+            text: "Sign-up failed !!",
+            textColor: "red",
+            backgroundColor: "#1b262c"
+        });
+    })
+};
+
+export const signIn = ( data )=>async( dispatch )=>{
+    console.log(object);
+    const { email, password } = data;
+
+    auth()
+    .signInWithEmailAndPassword( email, password )
+    .then(()=>{
+        console.log("Sign-in succesfuly !!!");
+        Snackbar.show({
+            text: "Sign-in succecesfuly !!",
+            textColor: "green",
+            backgroundColor: "#1b262c"
+        });
+    })
+    .catch(( error )=>{
+        console.error(error);
+        Snackbar.show({
+            text: "Sign-in failed !!!",
+            textColor: "red",
+            backgroundColor: "#1b262c"
+        });
+    });
+};
+
+export const signOut = ()=> async( dispatch )=>{
+    auth()
+    .signOut()
+    .then(()=>{
+        console.log("Sign-out succesfuly !!!");
+        Snackbar.show({
+            text: "Sign-out succecesfuly !!",
+            textColor: "green",
+            backgroundColor: "#1b262c"
+        });
+    })
+    .catch((error)=>{
+        console.error(error);
+        
+        Snackbar.show({
+            text: "Sign-out failed !!!",
+            textColor: "red",
+            backgroundColor: "#1b262c"
+        });
+    });
+};
